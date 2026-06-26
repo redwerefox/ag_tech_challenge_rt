106863a96774172b188dbfd5a7081e8afa9ef922
_Vulpes <vulpesk1tzune@gmail.com>
2026-06-27 00:26:54 +0200

    clean the windows sh

--- run_conan.bat
+++ run_conan.bat
@@ -1,7 +1,20 @@
+@echo off
+
+echo [1] Installing dependencies...
 conan install . --build=missing --profile profiles/windows-msvc
-build/Release/generators/conanbuildenv.bat
+
+echo [2] Activating Conan build environment...
+call build\Release\generators\conanbuild.bat
+
+echo [3] Configuring and Building...
 cmake --preset conan-release
 cmake --build --preset conan-release
-build/Release/generators/deactivate_conanbuildenv.bat
-build/Release/generators/conanrelease.bat
+
+echo [4] Switching to Runtime environment...
+call build\Release\generators\deactivate_conanbuild.bat
+call build\Release\generators\conanrun.bat
+
+echo [5] Running Tests...
 ctest --preset conan-release
+
+echo Done!
