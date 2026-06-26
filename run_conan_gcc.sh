77172117191c6109eaed78385d116c4ec5931f0d
_Vulpes <vulpesk1tzune@gmail.com>
2026-06-26 23:59:39 +0200

    set source commands to run script

--- run_conan_gcc.sh
+++ run_conan_gcc.sh
@@ -1,5 +1,8 @@
 
 conan install . --build=missing -s build_type=Release --profile=profiles/linux-gcc
+source build/Release/generators/conanbuild.sh
 cmake --preset conan-release
 cmake --build --preset conan-release
+source build/Release/generators/deactivate_conanbuildenv.sh
+source build/Release/generators/conanrelease.sh
 ctest --preset conan-release
