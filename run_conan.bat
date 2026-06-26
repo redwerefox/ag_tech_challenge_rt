a0f0aa3c162daef7e7d4c6eeb9cb9cc3f2d10d33
_Vulpes <vulpesk1tzune@gmail.com>
2026-06-26 19:06:23 +0200

    remove generator activations, from bat it seems to work

--- run_conan.bat
+++ run_conan.bat
@@ -1,7 +1,6 @@
 conan install . --build=missing --profile profiles/windows-msvc
-call build\Release\generators\conanbuild.bat
+
 cmake --preset conan-release
 cmake --build --preset conan-release
-call build\Release\generators\deactivate_conanbuild.bat
-call build\Release\generators\conanrun.bat
+
 ctest --preset conan-release
