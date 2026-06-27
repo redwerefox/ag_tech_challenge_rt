bcada818f015845992616a8228ddbbd02c60c4cf
_Vulpes <vulpesk1tzune@gmail.com>
2026-06-27 19:42:48 +0200

    suggested improvements for the actions: return codes in failures + more echos + conan profile detect

--- run_conan.bat
+++ run_conan.bat
@@ -2,13 +2,17 @@
 
 echo [1] Installing dependencies...
 conan install . --build=missing --profile profiles/windows-msvc
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 echo [2] Activating Conan build environment...
 call build\Release\generators\conanbuild.bat
 
+
 echo [3] Configuring and Building...
 cmake --preset conan-release
+if %errorlevel% neq 0 exit /b %errorlevel%
 cmake --build --preset conan-release
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 echo [4] Switching to Runtime environment...
 call build\Release\generators\deactivate_conanbuild.bat
@@ -16,5 +20,6 @@ call build\Release\generators\conanrun.bat
 
 echo [5] Running Tests...
 ctest --preset conan-release
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 echo Done!
