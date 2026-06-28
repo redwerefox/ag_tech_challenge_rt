54a4d4d8b6bae69761d49f3a8ff83ccd9e2508e3
_Vulpes <vulpesk1tzune@gmail.com>
2026-06-28 21:46:40 +0200

    staging a first attempt to add tidy and format checking to the project

--- run_conan_clang_checkFormat.sh
+++ run_conan_clang_checkFormat.sh
@@ -3,6 +3,6 @@
 set -e
 
 conan install . --build=missing -s build_type=Release --profile:build=profiles/linux-clang --profile:host=profiles/linux-clang
-cmake --preset conan-release -DRUN_CLANG_TIDY=OFF
+cmake --preset conan-release -DENABLE_FORMAT_CHECK=ON
 cmake --build --preset conan-release
 ctest --preset conan-release
