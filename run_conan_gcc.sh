a6d08715448d3efaa7f5a2dd2dbab162d4d2ae55
Vulpes <vulpesk1tzune@gmail.com>
2026-06-30 21:11:06 +0200

    final fix attempt for gcc -> set build env

--- run_conan_gcc.sh
+++ run_conan_gcc.sh
@@ -4,6 +4,7 @@ set -e
 
 echo "[1] conan install..."
 conan install . --build=missing -s build_type=Release --profile:build=profiles/linux-gcc --profile:host=profiles/linux-gcc
+source build/Release/generators/conanbuild.sh
 cmake --preset conan-release
 cmake --build --preset conan-release
 ctest --preset conan-release
